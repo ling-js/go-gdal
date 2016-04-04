2d00bcf65d732d7d5f4bb2e920986cedb3cbbcf2
Kyle Shannon <kyle@pobox.com>
2016-04-04 13:02:15 -0600

    initial gridding algorithm, options still needs a little work

--- algorithms.go
+++ algorithms.go
@@ -10,8 +10,22 @@ package gdal
 #cgo windows CFLAGS: -IC:/gdal/release-1600-x64/include
 */
 import "C"
-import (
-	"unsafe"
+import "unsafe"
+
+type GridAlgorithm int
+
+const (
+	InverseDistanceToAPower                = GridAlgorithm(C.GGA_InverseDistanceToAPower)
+	MovingAverage                          = GridAlgorithm(C.GGA_MovingAverage)
+	NearestNeighbor                        = GridAlgorithm(C.GGA_NearestNeighbor)
+	MetricMinimum                          = GridAlgorithm(C.GGA_MetricMinimum)
+	MetricMaximum                          = GridAlgorithm(C.GGA_MetricMaximum)
+	MetricRange                            = GridAlgorithm(C.GGA_MetricRange)
+	MetricCount                            = GridAlgorithm(C.GGA_MetricCount)
+	MetricAverageDistance                  = GridAlgorithm(C.GGA_MetricAverageDistance)
+	MetricAverageDistancePts               = GridAlgorithm(C.GGA_MetricAverageDistancePts)
+	Linear                                 = GridAlgorithm(C.GGA_Linear)
+	InverseDistanceToAPowerNearestNeighbor = GridAlgorithm(C.GGA_InverseDistanceToAPowerNearestNeighbor)
 )
 
 /* --------------------------------------------- */
@@ -298,6 +312,196 @@ func (src RasterBand) SieveFilter(
 /* --------------------------------------------- */
 /* Gridding functions                            */
 /* --------------------------------------------- */
+// The option structs are fully public.
 
-//Unimplemented: CreateGrid
+type InverseDistanceToAPowerOptions struct {
+	Power           float64
+	Smoothing       float64
+	AnisotropyRatio float64
+	AnisotropyAngle float64
+	Radius1         float64
+	Radius2         float64
+	Angle           float64
+	MaxPoints       uint32
+	MinPoints       uint32
+	NoDataValue     float64
+	//C.GDALGridInverseDistanceToAPowerOptions
+}
+
+func (o InverseDistanceToAPowerOptions) gridOption() unsafe.Pointer {
+	c := C.GDALGridInverseDistanceToAPowerOptions{
+		dfPower:           C.double(o.Power),
+		dfSmoothing:       C.double(o.Smoothing),
+		dfAnisotropyRatio: C.double(o.AnisotropyRatio),
+		dfAnisotropyAngle: C.double(o.AnisotropyAngle),
+		dfRadius1:         C.double(o.Radius1),
+		dfRadius2:         C.double(o.Radius2),
+		dfAngle:           C.double(o.Angle),
+		nMaxPoints:        C.GUInt32(o.MaxPoints),
+		nMinPoints:        C.GUInt32(o.MinPoints),
+		dfNoDataValue:     C.double(o.NoDataValue)}
+
+	return unsafe.Pointer(&c)
+}
+
+type InverseDistanceToAPowerNearestNeighborOptions struct {
+	Power       float64
+	Radius      float64
+	MaxPoints   uint32
+	MinPoints   uint32
+	NoDataValue float64
+	//C.GDALGridInverseDistanceToAPowerNearestNeighborOptions
+}
+
+func (o InverseDistanceToAPowerNearestNeighborOptions) gridOption() unsafe.Pointer {
+	c := C.GDALGridInverseDistanceToAPowerNearestNeighborOptions{
+		dfPower:       C.double(o.Power),
+		dfRadius:      C.double(o.Radius),
+		nMaxPoints:    C.GUInt32(o.MaxPoints),
+		nMinPoints:    C.GUInt32(o.MinPoints),
+		dfNoDataValue: C.double(o.NoDataValue)}
+
+	return unsafe.Pointer(&c)
+}
+
+type MovingAverageOptions struct {
+	Radius1     float64
+	Radius2     float64
+	Angle       float64
+	MinPoints   uint32
+	NoDataValue float64
+	//C.GDALGridMovingAverageOptions
+}
+
+func (o MovingAverageOptions) gridOption() unsafe.Pointer {
+	c := C.GDALGridMovingAverageOptions{
+		dfRadius1:     C.double(o.Radius1),
+		dfRadius2:     C.double(o.Radius2),
+		dfAngle:       C.double(o.Angle),
+		nMinPoints:    C.GUInt32(o.MinPoints),
+		dfNoDataValue: C.double(o.NoDataValue)}
+
+	return unsafe.Pointer(&c)
+}
+
+type NearestNeighborOptions struct {
+	Radius1     float64
+	Radius2     float64
+	Angle       float64
+	NoDataValue float64
+	//C.GDALGridNearestNeighborOptions
+}
+
+func (o NearestNeighborOptions) gridOption() unsafe.Pointer {
+	c := C.GDALGridNearestNeighborOptions{
+		dfRadius1:     C.double(o.Radius1),
+		dfRadius2:     C.double(o.Radius2),
+		dfAngle:       C.double(o.Angle),
+		dfNoDataValue: C.double(o.NoDataValue)}
+
+	return unsafe.Pointer(&c)
+}
+
+type DataMetricsOptions struct {
+	Radius1     float64
+	Radius2     float64
+	Angle       float64
+	MinPoints   uint32
+	NoDataValue float64
+	//C.GDALGridDataMetricsOptions
+}
+
+func (o DataMetricsOptions) gridOption() unsafe.Pointer {
+	c := C.GDALGridDataMetricsOptions{
+		dfRadius1:     C.double(o.Radius1),
+		dfRadius2:     C.double(o.Radius2),
+		dfAngle:       C.double(o.Angle),
+		nMinPoints:    C.GUInt32(o.MinPoints),
+		dfNoDataValue: C.double(o.NoDataValue)}
+
+	return unsafe.Pointer(&c)
+}
+
+type LinearOptions struct {
+	Radius      float64
+	NoDataValue float64
+	//C.GDALGridLinearOptions
+}
+
+func (o LinearOptions) gridOption() unsafe.Pointer {
+	c := C.GDALGridLinearOptions{
+		dfRadius:      C.double(o.Radius),
+		dfNoDataValue: C.double(o.NoDataValue)}
+
+	return unsafe.Pointer(&c)
+}
+
+type gridOption interface {
+	gridOption() unsafe.Pointer
+}
+
+func GridCreate(
+	algorithm GridAlgorithm,
+	options gridOption,
+	x, y, z []float64,
+	xMin, xMax, yMin, yMax float64,
+	xSize, ySize uint32,
+	buffer interface{},
+	progress ProgressFunc,
+	pData interface{}) int {
+
+	var dataType DataType
+	var dataPtr unsafe.Pointer
+	var length int
+	switch data := buffer.(type) {
+	case []int8:
+		dataType = Byte
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	case []uint8:
+		dataType = Byte
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	case []int16:
+		dataType = Int16
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	case []uint16:
+		dataType = UInt16
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	case []int32:
+		dataType = Int32
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	case []uint32:
+		dataType = UInt32
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	case []float32:
+		dataType = Float32
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	case []float64:
+		dataType = Float64
+		dataPtr = unsafe.Pointer(&data[0])
+		length = len(data)
+	}
+
+	err := C.GDALGridCreate(
+		C.GDALGridAlgorithm(algorithm),
+		options.gridOption(),
+		C.GUInt32(length),
+		(*C.double)(unsafe.Pointer(&x[0])),
+		(*C.double)(unsafe.Pointer(&y[0])),
+		(*C.double)(unsafe.Pointer(&z[0])),
+		C.double(xMin), C.double(xMax),
+		C.double(yMin), C.double(yMax),
+		C.GUInt32(xSize), C.GUInt32(ySize),
+		C.GDALDataType(dataType), dataPtr,
+		nil, nil)
+
+	return int(err)
+}
+
 //Unimplemented: ComputeMatchingPoints
