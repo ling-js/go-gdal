ce9688097c7aa4637a672fe03934bd0f581bc60c
Kyle Shannon <kyle@pobox.com>
2016-10-17 19:12:44 -0600

    use doc comments from gdal for very stable functions and add HTTPEnabled()

--- cpl.go
+++ cpl.go
@@ -3,6 +3,7 @@ package gdal
 /*
 #include "go_gdal.h"
 #include "gdal_version.h"
+#include "cpl_http.h"
 
 #cgo linux  pkg-config: gdal
 #cgo darwin pkg-config: gdal
@@ -14,7 +15,35 @@ import (
 	"unsafe"
 )
 
-// Get a configuration option.
+// GetConfigOption
+//
+// Get the value of a configuration option.
+//
+// The value is the value of a (key, value) option set with
+// CPLSetConfigOption().  If the given option was no defined with
+// CPLSetConfigOption(), it tries to find it in environment variables.
+//
+// Note: the string returned by CPLGetConfigOption() might be short-lived, and
+// in particular it will become invalid after a call to CPLSetConfigOption()
+// with the same key.
+//
+// To override temporary a potentially existing option with a new value, you
+// can use the following snippet :
+//
+//     // backup old value
+//     const char* pszOldValTmp = CPLGetConfigOption(pszKey, NULL);
+//     char* pszOldVal = pszOldValTmp ? CPLStrdup(pszOldValTmp) : NULL;
+//     // override with new value
+//     CPLSetConfigOption(pszKey, pszNewVal);
+//     // do something useful
+//     // restore old value
+//     CPLSetConfigOption(pszKey, pszOldVal);
+//     CPLFree(pszOldVal);
+//
+//
+// see SetConfigOption()
+//
+// http://trac.osgeo.org/gdal/wiki/ConfigOptions
 func GetConfigOption(key, def string) string {
 	k := C.CString(key)
 	d := C.CString(def)
@@ -24,11 +53,45 @@ func GetConfigOption(key, def string) string {
 	return C.GoString(opt)
 }
 
-// Set a configuration option.
+// SetConfigOption
+//
+// Set a configuration option for GDAL/OGR use.
+//
+// Those options are defined as a (key, value) couple. The value corresponding
+// to a key can be got later with the CPLGetConfigOption() method.
+//
+// This mechanism is similar to environment variables, but options set with
+// CPLSetConfigOption() overrides, for CPLGetConfigOption() point of view,
+// values defined in the environment.
+//
+// If CPLSetConfigOption() is called several times with the same key, the
+// value provided during the last call will be used.
+//
+// Options can also be passed on the command line of most GDAL utilities
+// with the with '--config KEY VALUE'. For example,
+// ogrinfo --config CPL_DEBUG ON ~/data/test/point.shp
+//
+// This function can also be used to clear a setting by passing NULL as the
+// value (note: passing NULL will not unset an existing environment variable;
+// it will just unset a value previously set by CPLSetConfigOption()).
+//
+// see GetConfigOption
+//
+// http://trac.osgeo.org/gdal/wiki/ConfigOptions
 func SetConfigOption(key, value string) {
 	k := C.CString(key)
 	v := C.CString(value)
 	defer C.free(unsafe.Pointer(k))
 	defer C.free(unsafe.Pointer(v))
 	C.CPLSetConfigOption(k, v)
+}
+
+// HTTPEnabled checks to see if the underlying GDAL library has libcurl support
+// configured and built
+func HTTPEnabled() bool {
+	rc := C.CPLHTTPEnabled()
+	if int(rc) == 0 {
+		return false
+	}
+	return true
 }
